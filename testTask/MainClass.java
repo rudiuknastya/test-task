e7257bc620d0b4be35066746be80855a6ef429dc
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-04-15 11:21:13 +0300

    Update MainClass.java

--- testTask/MainClass.java
+++ testTask/MainClass.java
@@ -12,7 +12,6 @@ public class MainClass {
 		weekTrainings[1] = new RunTraining(rand.nextInt(101)+30,rand.nextInt(41)+ 60,rand.nextInt(61)+ 60,rand.nextInt(10)+1);
 		weekTrainings[2] = new CrossFitTraining(rand.nextInt(101)+30,rand.nextInt(41)+ 60,rand.nextInt(61)+ 60,rand.nextInt(20)+1);
 		weekTrainings[3] = new BoxTraining(rand.nextInt(101)+30,rand.nextInt(41)+ 60,rand.nextInt(61)+ 60);
-		System.out.println();
 		for(Training training:weekTrainings) {
 			training.printTraining();
 		}
